92cd69332079f3604b3ccd930895e907caeb9a42
Gabriel Felicio <137421493+XaauuziN@users.noreply.github.com>
2024-03-12 00:40:07 -0300

    5- Loja com %.java

--- 5- Loja com %.java
+++ 5- Loja com %.java
@@ -1,30 +1,24 @@
-package aula.um.e.dois;
-
 import javax.swing.JOptionPane;
 
-public class Exemplos {
+public class Atividade5 {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        System.out.println("Digite o valor do produto:");
-        double valorProduto = scanner.nextDouble();
-        
+        double valorProduto = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor do produto"));
+
         Produto produto = new Produto(valorProduto);
         produto.calcularLucro();
-        
-        System.out.println("Valor do lucro: R$" + produto.getLucro());
-        System.out.println("Preço de venda: R$" + produto.getPrecoVenda());
-        
-        scanner.close();
+
+        JOptionPane.showMessageDialog(null,
+                "Valor do lucro: R$ " + produto.lucro +
+                        "\nPreço de venda: R$ " + produto.precoVenda);
     }
 }
 
 class Produto {
-    private double valorProduto;
-    private double lucro;
-    private double precoVenda;
-    private static final double LUCRO_PERCENTUAL = 0.3; // 30%
+    double valorProduto;
+    double lucro;
+    double precoVenda;
+    static final double LUCRO_PERCENTUAL = 0.3; // 30%
 
     public Produto(double valorProduto) {
         this.valorProduto = valorProduto;
@@ -33,13 +27,5 @@ class Produto {
     public void calcularLucro() {
         lucro = valorProduto * LUCRO_PERCENTUAL;
         precoVenda = valorProduto + lucro;
-    }
-
-    public double getLucro() {
-        return lucro;
-    }
-
-    public double getPrecoVenda() {
-        return precoVenda;
     }
 }
